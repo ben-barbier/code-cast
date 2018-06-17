a62bd8e717668ecea9630a258de4d19b742ebbe8
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2018-06-17 02:53:00 +0200

    feat: display code-cast folder content

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 #! /usr/bin/env node
 
-console.log('code-cast will start...');
+const server = require('server');
+const {render} = server.reply;
+const portfinder = require('portfinder');
+const dirTree = require('directory-tree');
+const {getInstalledPathSync} = require('get-installed-path');
+
+const root = getInstalledPathSync('code-cast');
+const argv = require('minimist')(process.argv.slice(2));
+const folder = argv['_'][0] || '.';
+const port = argv['p'] || argv['port'] || 3000;
+
+portfinder.basePort = port;
+return portfinder.getPortPromise().then(freeport => listen(freeport));
+
+function listen(port) {
+    console.log(`code-cast will start on port ${port}...`);
+    const tree = dirTree(folder);
+
+    server({port: port, views: `${root}/views`}, ctx =>
+        render('index.hbs', {tree: JSON.stringify(tree)}));
+
+}
