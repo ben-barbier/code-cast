c8b434ad4aeaf069b10c2572aa0a86e3f476f5f7
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2018-06-17 16:49:12 +0200

    feat: display all IPv4 addresses

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const dirTree = require('directory-tree');
 const {getInstalledPathSync} = require('get-installed-path');
 const colors = require('colors');
 const opener = require('opener');
+const ifaces = require('os').networkInterfaces();
 
 const root = getInstalledPathSync('code-cast');
 const argv = require('minimist')(process.argv.slice(2));
@@ -33,7 +34,8 @@ portfinder.getPortPromise().then(freeport => listen(path, freeport, root));
 function listen(path, port, root) {
 
     const tree = dirTree(path);
-    displayStartingMessage(path, port);
+    const origins = getOrigins(port);
+    displayStartingMessage(path, origins);
     openBrowserWindowIfActivated();
 
     server({port: port, views: `${root}/views`}, ctx => {
@@ -42,10 +44,22 @@ function listen(path, port, root) {
 
 }
 
-function displayStartingMessage(path, port) {
+function getOrigins(port) {
+    const origins = [];
+    Object.keys(ifaces).forEach(function (dev) {
+        ifaces[dev].forEach(function (details) {
+            if (details.family === 'IPv4') {
+                origins.push(`http://${details.address}:${port}`.green);
+            }
+        });
+    });
+    return origins;
+}
+
+function displayStartingMessage(path, origins) {
     console.log('Starting up code-cast, serving '.yellow + path.green);
     console.log(`Available on:`.yellow);
-    console.log(`  http://127.0.0.1:${port}`);
+    origins.forEach(origin => console.log(`  ${origin}`));
     console.log(`Hit CTRL-C to stop the server`);
 }
 
