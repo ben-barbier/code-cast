a8a0c8b936aed2e4038cf9d5fd3c8a7225fc9f0b
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2018-06-17 13:11:03 +0200

    feat: display starting message

--- index.js
+++ index.js
@@ -5,20 +5,31 @@ const {render} = server.reply;
 const portfinder = require('portfinder');
 const dirTree = require('directory-tree');
 const {getInstalledPathSync} = require('get-installed-path');
+const colors = require('colors');
 
 const root = getInstalledPathSync('code-cast');
 const argv = require('minimist')(process.argv.slice(2));
-const folder = argv['_'][0] || '.';
+const path = argv['_'][0] || '.';
 const port = argv['p'] || argv['port'] || 3000;
 
 portfinder.basePort = port;
-return portfinder.getPortPromise().then(freeport => listen(freeport));
+portfinder.getPortPromise().then(freeport => listen(path, freeport, root));
 
-function listen(port) {
-    console.log(`code-cast will start on port ${port}...`);
-    const tree = dirTree(folder);
+function listen(path, port, root) {
 
-    server({port: port, views: `${root}/views`}, ctx =>
-        render('index.hbs', {tree: JSON.stringify(tree)}));
+    const tree = dirTree(path);
+    displayStartingMessage(path, port);
+
+    server({port: port, views: `${root}/views`}, ctx => {
+        return render('index.hbs', {tree: JSON.stringify(tree)});
+    });
 
 }
+
+function displayStartingMessage(path, port) {
+    console.log('Starting up code-cast, serving '.yellow + path.green);
+    console.log(`Available on:`.yellow);
+    console.log(`  http://127.0.0.1:${port}`);
+    console.log(`Hit CTRL-C to stop the server`);
+}
+
